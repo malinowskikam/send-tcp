0011b73af1749dc2e7a4c686bee0ddd94de4194b
Kamil Malinowski <malinowskikam96@gmail.com>
2023-08-23 22:44:04 +0200

    Input reading raw only

--- src/args.rs
+++ src/args.rs
@@ -5,13 +5,14 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 pub struct Args {
-    /// Address of a open TCP socket
-    #[arg(index = 1)]
-    address: String,
 
-    /// Source file
+    /// Address of a open TCP socket as <IP>:<PORT>
+    #[arg(index = 1)]
+    pub address: String,
+
+    /// Input file path
     #[arg(index = 2)]
-    file: Option<PathBuf>,
+    pub file_path: Option<PathBuf>,
 }
 
 pub fn parse_args() -> Args {
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,37 @@
 mod args;
 
+use std::{io::Read, fs::File, process::exit};
+use args::{parse_args, Args};
+use anyhow::{Result, Error};
 
-use args::parse_args;
+const EXIT_INPUT_FAILED: i32 = 1;
 
 fn main() {
     let args = parse_args();
-    println!("{:?}", args);
+    println!("{:?}", &args);
+
+    let input = match get_input(&args) {
+        Ok(input) => input,
+        Err(e) => exit_with_error(e, EXIT_INPUT_FAILED)
+    };
+    
+    println!("{:?}", std::str::from_utf8(&input).unwrap())    
+}
+
+fn exit_with_error(error: Error, code: i32) -> ! {
+    println!("error: {}", error);
+    exit(code)
+}
+
+fn get_input(args: &Args) -> Result<Vec<u8>> {
+    let mut input: Vec<u8> = Vec::new();
+
+    if let Some(file_path) = &args.file_path {
+        let mut file = File::open(file_path)?;
+        file.read_to_end(&mut input)?;
+    } else {        
+        std::io::stdin().lock().read_to_end(&mut input)?;
+    };
+
+    Ok(input)
 }
