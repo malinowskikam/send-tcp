94eec2e87b4982da247c39191ade9707afb41814
Kamil Malinowski <malinowskikam96@gmail.com>
2023-08-23 23:25:06 +0200

    Write, test listener added

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,53 @@
 mod args;
 
-use std::{io::Read, fs::File, process::exit};
+use std::{io::{Read, Write}, fs::File, process::exit, net::{TcpStream, TcpListener}, time};
 use args::{parse_args, Args};
 use anyhow::{Result, Error};
 
 const EXIT_INPUT_FAILED: i32 = 1;
+const EXIT_CONN_FAILED: i32 = 2;
+const EXIT_WRITE_FAILED: i32 = 3;
 
 fn main() {
+    let handle = std::thread::spawn(|| {
+        let listener = TcpListener::bind("127.0.0.1:22000").unwrap();
+        println!("listening");
+        let mut stream = listener.accept().unwrap().0;
+        
+        println!("reading");
+        let mut buff = String::new();
+        stream.read_to_string(&mut buff).unwrap();
+        println!("{}", buff);
+    });
+
+    std::thread::sleep(time::Duration::from_secs(1));
+
+
     let args = parse_args();
-    println!("{:?}", &args);
 
     let input = match get_input(&args) {
         Ok(input) => input,
         Err(e) => exit_with_error(e, EXIT_INPUT_FAILED)
     };
+    println!("{}", input.len());
     
-    println!("{:?}", std::str::from_utf8(&input).unwrap())    
+    let mut tcp_stream = match TcpStream::connect(&args.address) {
+        Ok(stream) => stream,
+        Err(e) => exit_with_error(e.into(), EXIT_CONN_FAILED)
+    };
+    println!("connected");
+
+
+    std::thread::sleep(time::Duration::from_secs(1));
+
+    match tcp_stream.write_all(&input) {
+        Ok(_) => {
+            println!("written");
+            tcp_stream.shutdown(std::net::Shutdown::Both).unwrap();
+            handle.join().unwrap()
+        },
+        Err(e) => exit_with_error(e.into(), EXIT_WRITE_FAILED)
+    };
 }
 
 fn exit_with_error(error: Error, code: i32) -> ! {
